901122bb909e3ff9e87240adabb04a57172791f7
vikash <vikashkumar04081999@gmail.com>
2024-06-20 13:28:06 +0530

    done changes

--- server.js
+++ server.js
@@ -24,6 +24,6 @@ const dbConnect = dbConnection(mongoUri)
 if (dbConnect) {
     // Start the server
     app.listen(port, () => {
-        console.log(`Server running on port ${port}`);
+        console.log(`Server running on port : ${port}`);
     });
 }
